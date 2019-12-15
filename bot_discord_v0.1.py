71ae6aec93c1a31351e31dc44e896f6575b6cf24
m1zan <56271346+m1zan@users.noreply.github.com>
2019-12-15 15:35:56 +0300

    Add files via upload

---
+++ bot_discord_v0.1.py
@@ -0,0 +1,21 @@
+import discord  # импортируем библиотеку дс
+from discord.ext import commands
+from discord.ext.commands import Bot
+import os
+
+Bot = commands.Bot(command_prefix='!')
+
+@Bot.event
+async def on_ready():
+    print('Bot is online and We have logged in as {0.user}' .format(Bot))
+@Bot.command(pass_context=True)
+async def hello(ctx):
+    await ctx.send('Greetings, mortal')
+
+@Bot.command()
+@commands.has_permissions(administrator=True)
+async def mute(ctx, member: discord.Member):
+    mute_role = discord.utils.get(ctx.message.guild.roles, name='mute')
+    await member.add_roles(mute_role)
+
+token = os.environ.get('token')
